0775f07177cfb64826794ae6c8b30963da47e760
zeeshan038 <hankhan123623@gmail.com>
2025-07-02 17:40:54 +0500

    deploy fix

--- src/constant.js
+++ src/constant.js
@@ -1,3 +1,3 @@
-export const BASE_URL = "https://alshifa-pharma-ddahf7f8ehhcgre6.canadacentral-01.azurewebsites.net"
+export const BASE_URL = "https://new-alshifa-backend-2.onrender.com"
 // export const BASE_URL = "http://localhost:5000"
 
